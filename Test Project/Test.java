7145fb9632397c9187073d9a4493365b5da8b0f0
CLaporte <claporte7850@students.mc3.edu>
2018-03-28 21:11:54 -0400

    First commit to test

--- Test Project/Test.java
+++ Test Project/Test.java
@@ -1,2 +1,5 @@
 //This file is simply to test push capability with Eclipse/Github Integration
 // testing also with the GitHub user that is already configured in my IDE. (oriel)
+
+// This is to test my push capability. I'm trying IntelliJ but if it seems to screw things up, I'm happy
+// to switch to Eclipse. (Chrissa)
