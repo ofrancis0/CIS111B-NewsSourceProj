9a8053ff4877a9cdde14afe82f5a2683bcac8abd
jmarz3 <jmarz@ml1.net>
2018-04-11 17:41:20 -0400

    Add files via upload
    Found connectivity success with java class to connect to MySQL database over ssh. Tho I suspect it is not totally clean. Included a screenshot of debug window to show the process. jsch.jar file included as the external jar necessary for this task.

---
+++ MySqlConnOverSSH2.java
@@ -0,0 +1,67 @@
+//package com.journaldev.java.ssh
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import java.sql.Connection;
+
+
+public class MySqlConnOverSSH2 {
+
+	/**
+	 * Java Program to connect to remote database through SSH using port forwarding
+	 * @author Pankaj@JournalDev
+	 * @throws SQLException 
+	 */
+   public static void main(String[] args) throws SQLException {
+   
+      int lport=3306;
+      String rhost="104.131.100.240";
+      String host="104.131.100.240";
+      int rport=3306;
+      String user="chrissa";
+      String password="LaP0r#e!";
+      String dbuserName = "chrissa";
+      String dbpassword = "LaP0r#e!";
+      String url = "jdbc:mysql://104.131.100.240:"+lport+"/news_test";
+      String driverName="com.mysql.jdbc.Driver";
+      Connection conn = null;
+      Session session= null;
+      try{
+       	//Set StrictHostKeyChecking property to no to avoid UnknownHostKey issue
+         java.util.Properties config = new java.util.Properties(); 
+         config.put("StrictHostKeyChecking", "no");
+         JSch jsch = new JSch();
+         session=jsch.getSession(user, host, 22);
+         session.setPassword(password);
+         session.setConfig(config);
+         session.connect();
+         System.out.println("Connected");
+         int assinged_port=session.setPortForwardingL(lport, rhost, rport);
+         System.out.println("localhost:"+assinged_port+" -> "+rhost+":"+rport);
+         System.out.println("Port Forwarded");
+       	
+       	//mysql database connectivity
+         Class.forName(driverName).newInstance();
+         conn = DriverManager.getConnection (url, dbuserName, dbpassword);
+         System.out.println ("Database connection established");
+         System.out.println("DONE");
+      }catch(Exception e){
+         e.printStackTrace();
+      }finally{
+         if(conn != null && !conn.isClosed()){
+            System.out.println("Closing Database Connection");
+            conn.close();
+         }
+         if(session !=null && session.isConnected()){
+            System.out.println("Closing SSH Connection");
+            session.disconnect();
+         }
+      }
+   }
+
+}
