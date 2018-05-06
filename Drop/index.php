76562751ad51a6a9b884330bc58f294e815a3fda
jmarz3 <jmarz@ml1.net>
2018-05-05 21:18:58 -0400

    Final PHP commit

--- Drop/index.php
+++ Drop/index.php
@@ -1,57 +1,57 @@
 <?php
-// put your code here
-//include 'inc/DBConn.php';
-//include 'inc/DBSelectDrop.php';
-//include 'inc/DBNewsTitles.php';
-//include 'inc/DBViewNewsData.php';
-
+/*
+ * credentials used to connect to MySQL database
+ * @var string
+ */
 $dbServ = "localhost";
 $dbUser = "phpusr";
 $dbPass = "!php#usr!";
 $dbName = "news_test";
 
-//$showtable = false;
+/*
+ * initialized variable with default value used in conditional statement to 
+ * determine if query renders results and table data is generated. 
+ */
+$showtable = false; 
 
-$msg = '';
-$msg_records = '';
+$msg = '';  // variable for message confirming the source (topic) records were retrieved from.
+$msg_records = '';  // variable for message confirming number of records retrieved.
+$result;    // variable to hold results of returned query results.
+$numRows;   // variable to hold results of variable used to hold count of records returned.
 
-$result;
-//$result2='';
-$numRows;
-
-
+/*
+ * variable to hold returned data from instantiated mysqli built-in function 
+ * to authenticate to MySQL database.
+ */
 $conn = new mysqli($dbServ, $dbUser, $dbPass, $dbName);
 
 //if there is an error with connectivity this conditional statement will inform.
 if ($conn->connect_errno) {
     exit("Database Connection Failed. Reason: " . $conn->connect_error);
 } else {
-    echo "Connection to Database Successful<br><br>";
-    //return $conn;
-}
+    //echo "Connection to Database Successful<br><br>";
+    }
 
 // Set character set from default latin1 to utf8 for successful translation of dbase characters.
 $conn->set_charset("utf8");
-printf("Initial character set: %s\n", $conn->character_set_name()). "<br><br>";
+//printf("Initial character set: %s\n", $conn->character_set_name(). "<br><br>");
 
-//function getDropData() {
-//Our select statement. This will retrieve the data that we want.
-//$sql = "SELECT title, author, description, website_name, url FROM "
-//        . "news_item ORDER BY title ASC";
-//$sql = "SELECT DISTINCT topic FROM dummy_article ORDER BY topic ASC";
-/*$sql = "SELECT DISTINCT a1.topic FROM news_test.dummy_article a1 INNER JOIN (SELECT topic FROM"
-. " news_test.dummy_article GROUP BY topic ORDER BY COUNT(*) DESC LIMIT 5) AS a2 ON a1.topic = a2.topic"
-. " ORDER BY a1.topic;";
-*/
+/*
+ * variable to store result of query used to retrieve the data 
+ * from the MySQL database.
+ * @var sql
+ */ 
 $sql = "SELECT DISTINCT article.topic FROM news_test.article WHERE article.topic " .
-       "IS NOT NULL and article.topic <>'' ORDER BY article.topic;"
-$sqlTot = "SELECT * FROM dummy_article ORDER BY topic ASC";
+       "IS NOT NULL and article.topic <>'' ORDER BY article.topic";
+
+// query to get a total count of records in database.
+$sqlTot = "SELECT * FROM article ORDER BY topic ASC";
 
 /*
  * use the connect() method from the DBConn class to connect to database,
- * run the query, retrieve results and store in $results variable.
+ * run the query, retrieve results and store in $result variable.
  */
-$result = $conn->query($sql); // conn was this->connect().
+$result = $conn->query($sql); 
 $recordCount = $conn->query($sqlTot);
 
 /*
@@ -59,9 +59,8 @@ $recordCount = $conn->query($sqlTot);
  *  num_rows is a built in php function to return the count of
  *  the number of rows returned.
  */
-//$numRows=$result->num_rows; 
 //show number of records returned
-echo "There are " . $recordCount->num_rows . " total records in this set<br>";
+//echo "There are " . $recordCount->num_rows . " total records in this set<br>";
 echo "There are " . $result->num_rows . " distinct topics<br>"; // result was numRows
 echo "<hr></hr>";
 
@@ -76,7 +75,9 @@ $numRows = $result->num_rows;
 /*
  * a conditional loop to check that there are indeed rows/data that are 
  * available for processing. If rows exist then to process them and pass
- * into an associative array then returned for further processing.
+ * into an associative array then returned for further processing,
+ * also set $showtable variable boolean to true to allow creation of html 
+ * table.
  */
 if ($numRows > 0) {
 
@@ -84,12 +85,12 @@ if ($numRows > 0) {
     $showtable = true;
     $msg = "Results from " . $topic;   // test the value is correct
     // second sql statement
-    $sql2 = "SELECT title, sources, pubdate, url FROM dummy_article WHERE topic = '" . $topic . "'";
+    $sql2 = "SELECT title, sources, pubdate, url FROM article WHERE topic = '" . $topic . "'";
 
     // execute the second result set
     $result2 = $conn->query($sql2);
 
-    // Show number of records returned
+    // variable to show number of records returned
     $msg_records = "<em>" . $result2->num_rows . " records from this drop selection</em><br><br>";
 }
 ?>
@@ -97,13 +98,15 @@ if ($numRows > 0) {
 
 <!DOCTYPE html>
 <!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+The index.php file is the web front end of the NewsApp project that hosts the 
+html page that presents the news topics and selection of queried topics for
+the user to interact withal
+
+@author Oriel Francis, Chrissa LaPorte, Aaron Wile.
 -->
 <html>
     <head>
-        <meta charset="UTF-16">
+        <meta charset="UTF-8">
         <title>Query the Dropdown from Topic Selection</title>
     </head>
 
@@ -135,16 +138,18 @@ and open the template in the editor.
         <h1>Query the Topics</h1>
         <p>
 
-<?php echo $msg_records; ?></p>
+<?php echo $msg_records; ?></p> 
 
 <?php
 //populate the drop down list
 if ($result->num_rows > 0) {  // error check. Ensure there are rows to process.
     echo "<form action='index.php' method='post'>\n";
     echo "<select name='topic' id='topic'>\n";
-    echo "<option value='none'>Please Select a Topic</option>\n";
+    echo "<option value='none'>Select a Topic</option>\n";
 
-    // output data of each row
+    /*
+     *  output data of each row
+     */
     while ($row = $result->fetch_assoc()) {
         echo "<option value='" . $row["topic"] . "'>" . $row["topic"] . "</option>\n";
     }   //end while
@@ -156,13 +161,15 @@ if ($result->num_rows > 0) {  // error check. Ensure there are rows to process.
 else {
     echo "Sorry, there were no results";
 }   // end else
-// display query if showtable variable is true.
+
+/*
+ * generate result table if showtable variable is true else inform user 
+ * that no results are found.
+ */
 if ($showtable == true) {
 
     //<!--second sql2 query table results -->
-
     if ($result2->num_rows > 0) {
-
         echo"<table>"
         . "<thead>"
         . "<tr>"
@@ -174,7 +181,7 @@ if ($showtable == true) {
         . "</thead>"
         . "<tbody>\n\n";
 
-        // output data of each row
+        // processed output data of each row
         while ($row2 = $result2->fetch_assoc()) {
             echo "<tr>\n";
             echo "<td>" . $row2["title"] . "</td>\n";
@@ -187,12 +194,16 @@ if ($showtable == true) {
         echo "</tbody>\n\n</table>\n";
     } // end if
     else {
-        echo "Sorry, there were no results";
+        echo "Sorry, there were no results"; // output if no table results are found.
     } // end else
 } // end outer if
 ?>
 
-        <p> <?php echo $msg; ?> </p>
+        <!-- user info output of number of records found in chosen topic -->
+        <p> <?php echo $msg; ?> </p> 
+        
+        <!-- attribution to NewsAPI -->
+        <p> <a href="https://newsapi.org" target=\"_blank\">Powered by NewsAPI.org</a></p>
 
     </body>
 </html> 
